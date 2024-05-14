4b44fd7eff57c4ede3f55ef24edf7d4c0b717fa0
Warzinhuw <rafael_martorano@hotmail.com>
2024-05-14 10:28:36 -0300

    feat: initial commit

---
+++ Movement.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@export var speed = 400
+var screen_size # Size of the game window.
+
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
